b56a3e9e69fb2ffe1994e046d2c0e58920c3d476
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-01 14:34:51 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,38 @@
-function Header() {
-    return (
-        <header id="header">
-            <nav id="nav">
-                <div id="menu-tab"></div>
-                <div id="menu-toggle">
-                    <input name="menu-button-click" type="checkbox" />
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <ul id="menu">
-                        <li>
-                            <a href="#home">Home</a>
-                        </li>
-                        <li>
-                            <hr class="nav-division" />
-                        </li>
-                        <li>
-                            <a href="#about">About</a>
-                        </li>
-                        <li>
-                            <hr class="nav-division" />
-                        </li>
-                        <li>
-                            <a href="#resume">Resume</a>
-                        </li>
-                        <li>
-                            <hr class="nav-division" />
-                        </li>
-                        <li>
-                            <a href="#projects">Projects</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    );
+export default function Header() {
+  return (
+    <header id="header">
+      <nav id="nav">
+        <div id="menu-tab"></div>
+        <div id="menu-toggle">
+          <input name="menu-button-click" type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <hr class="nav-division" />
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <hr class="nav-division" />
+            </li>
+            <li>
+              <a href="#education">Education</a>
+            </li>
+            <li>
+              <hr class="nav-division" />
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
-
-export default Header;
