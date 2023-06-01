beb0a7da1bbadb80dcf0ff011f865a101fba88c6
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-01 14:35:17 -0400

    Redesign Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,28 +1,34 @@
-function Education({ data }) {
-    return (
-        <>
-            <h3 className="experience-name">{data.institute}</h3>
-            <h4>{data.location}</h4>
-            <h4>
-                {new Date(data.started_at).toLocaleDateString("en-US", {
-                    month: "short",
-                    year: "numeric",
-                })}
-                {data.ended_at
-                    ? ` - ${new Date(data.ended_at).toLocaleDateString(
-                          "en-US",
-                          { month: "short", year: "numeric" }
-                      )}`
-                    : " - Present"}
-            </h4>
-            <h4>{data.qualification}</h4>
-            <ul>
-                {data.content.split(";").map((bullet, index) => (
-                    <li key={index}>{bullet}</li>
-                ))}
-            </ul>
-        </>
-    );
+export default function Education({ data }) {
+  return (
+    <>
+      {/* <h3 className="experience-name">{data.institute}</h3>
+      <h4>{data.location}</h4>
+      <h4>
+        {new Date(data.started_at).toLocaleDateString("en-US", {
+          month: "short",
+          year: "numeric",
+        })}
+        {data.ended_at
+          ? ` - ${new Date(data.ended_at).toLocaleDateString("en-US", {
+              month: "short",
+              year: "numeric",
+            })}`
+          : " - Present"}
+      </h4>
+      <h4>{data.qualification}</h4>
+      <ul>
+        {data.content.split(";").map((bullet, index) => (
+          <li key={index}>{bullet}</li>
+        ))}
+      </ul> */}
+      <div className="institute-card">
+        <img
+          className="institute-emblem"
+          src={data.image}
+          alt={`Emblem of ${data.institute}.`}
+        />
+        <h2 className="institute-name">{data.institute}</h2>
+      </div>
+    </>
+  );
 }
-
-export default Education;
