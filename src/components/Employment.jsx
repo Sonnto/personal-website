14d1605a563086e9db079bc7022441f2d6e80347
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-01 14:34:53 -0400

    Update Employment.jsx

--- src/components/Employment.jsx
+++ src/components/Employment.jsx
@@ -1,28 +1,26 @@
-function Employment({ data }) {
-    return (
-        <>
-            <h3 className="experience-name">{data.title}</h3>
-            <h3 className="experience-name">{data.employer}</h3>
-            <h4>{data.location}</h4>
-            <h4>
-                {new Date(data.started_at).toLocaleDateString("en-US", {
-                    month: "short",
-                    year: "numeric",
-                })}
-                {data.ended_at
-                    ? ` - ${new Date(data.ended_at).toLocaleDateString(
-                          "en-US",
-                          { month: "short", year: "numeric" }
-                      )}`
-                    : " - Present"}
-            </h4>
-            <ul>
-                {data.content.split(";").map((bullet, index) => (
-                    <li key={index}>{bullet}</li>
-                ))}
-            </ul>
-        </>
-    );
+export default function Employment({ data }) {
+  return (
+    <>
+      <h3 className="experience-name">{data.title}</h3>
+      <h3 className="experience-name">{data.employer}</h3>
+      <h4>{data.location}</h4>
+      <h4>
+        {new Date(data.started_at).toLocaleDateString("en-US", {
+          month: "short",
+          year: "numeric",
+        })}
+        {data.ended_at
+          ? ` - ${new Date(data.ended_at).toLocaleDateString("en-US", {
+              month: "short",
+              year: "numeric",
+            })}`
+          : " - Present"}
+      </h4>
+      <ul>
+        {data.content.split(";").map((bullet, index) => (
+          <li key={index}>{bullet}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
-
-export default Employment;
