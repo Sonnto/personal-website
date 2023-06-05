eb0d97f2b36910187d696bf3e4d366a3c2c4055a
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-05 15:59:49 -0400

    remove comments on unused education & employments

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,26 +1,6 @@
 export default function Education({ data }) {
   return (
     <>
-      {/* <h3 className="experience-name">{data.institute}</h3>
-      <h4>{data.location}</h4>
-      <h4>
-        {new Date(data.started_at).toLocaleDateString("en-US", {
-          month: "short",
-          year: "numeric",
-        })}
-        {data.ended_at
-          ? ` - ${new Date(data.ended_at).toLocaleDateString("en-US", {
-              month: "short",
-              year: "numeric",
-            })}`
-          : " - Present"}
-      </h4>
-      <h4>{data.qualification}</h4>
-      <ul>
-        {data.content.split(";").map((bullet, index) => (
-          <li key={index}>{bullet}</li>
-        ))}
-      </ul> */}
       <div className="institute-card">
         <img
           className="institute-emblem"
