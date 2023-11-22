4a586c3b6ca213c7b785d4875a225162ad02fc27
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-11-21 19:09:48 -0500

    Slight redesign and content change
    - redesign home
    - update subheadings
    - update x (fka twitter) social media icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,43 @@
-import AnimatedTitle from "./AnimatedTitle";
-
 export default function Home() {
   return (
     <>
+      <h1 className="main-title">
+        Kee-Fung
+        <span className="next-line">Anthony Ho</span>
+      </h1>
       <div className="emblem-item">
         <img
           className="emblem"
-          src="./assets/images/profile/placeholder-emblem.png"
-          alt="Placeholder emblem for website."
+          src="./assets/images/profile/anthony-side-profile.jpg"
+          alt="Headshot of Anthony"
         />
       </div>
-      <AnimatedTitle />
       <div className="subtext">
-        <p>Front-End • Back-End</p>
-        <p>UX • Design • UI</p>
-        <p>Web • Mobile • Apps</p>
+        <p>Political Scientist </p>
+        <p>Web Developer</p>
+        <p>Legal Professional</p>
       </div>
       <div className="social-icons">
         <a
           href="https://github.com/sonnto"
-          aria-label="Click for Anthony's GitHub"
+          aria-label="Click for Anthony's GitHub account"
           rel="noopener"
         >
           <i className="fab fa-github-square"></i>
         </a>
         <a
           href="https://linkedin.com/in/anthonykfho"
-          aria-label="Click for Anthony's LinkedIn"
+          aria-label="Click for Anthony's LinkedIn account"
           rel="noopener"
         >
           <i className="fab fa-linkedin"></i>
         </a>
         <a
-          href="https://twitter.com/anthonykfho"
-          aria-label="Click for Anthony's Twitter"
+          href="https://x.com/anthonykfho"
+          aria-label="Click for Anthony's X account"
           rel="noopener"
         >
-          <i className="fab fa-twitter-square"></i>
+          <i className="fab fa-square-x-twitter"></i>
         </a>
       </div>
     </>
