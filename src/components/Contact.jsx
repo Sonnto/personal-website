825f8d972824cd3dbd18916ee09d6b2bc3285155
Sonnto <90372802+Sonnto@users.noreply.github.com>
2024-02-05 14:59:11 -0500

    Remove resume download and phone number

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,5 @@
 export default function Contact({ data }) {
   const mailToEmail = `mailto:${data.email}`;
-  const telMobileNum = `tel:${data.mobileNum}`;
-  const viewCV = () => {
-    window.open("./assets/documents/CV_HoKeeFungAnthony.pdf", "_blank");
-  };
   return (
     <>
       <div className="contact-info">
@@ -11,31 +7,12 @@ export default function Contact({ data }) {
           <i className="fas fa-envelope"></i>
           <a
             href={mailToEmail}
-            aria-label="E-mail Anthony via his e-mail address"
+            aria-label="Contact Anthony via his e-mail address"
             rel="noopener"
           >
             &nbsp;&nbsp;&nbsp;{data.email}
           </a>
         </p>
-        <p>
-          <i className="fas fa-mobile-alt"></i>
-          <a
-            href={telMobileNum}
-            aria-label="Call Anthony via his mobile phone number"
-            rel="noopener"
-          >
-            &nbsp;&nbsp;&nbsp;{data.mobileNum}
-          </a>
-        </p>
-        <div className="cv-button-container">
-          <button
-            className="cv-button"
-            aria-label="View Anthony's CV in a new tab"
-            onClick={viewCV}
-          >
-            View CV
-          </button>
-        </div>
       </div>
     </>
   );
